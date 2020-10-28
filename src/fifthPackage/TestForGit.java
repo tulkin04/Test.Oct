03d51b30ae42d798d18c1dbeafdcf7d4be401335
Tulkin Norkulov <tulkinnorkulov92@gmail.com>
2020-10-28 15:19:01 -0400

    Second commit

---
+++ src/fifthPackage/TestForGit.java
@@ -0,0 +1,32 @@
+package fifthPackage;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestForGit {
+	@Test
+	public void test1() {
+		System.out.println("Test 1");
+		
+	}
+	@Test
+	public void test2() {
+		System.out.println("Test 2");
+		
+	}
+	@Test
+	public void test3() {
+		System.out.println("Test 3");
+		Assert.assertTrue(false);
+	}
+	@Test
+	public void test4() {
+		System.out.println("Test 4");	
+	}
+	@Test
+	public void testforgit() {
+		System.out.println("test for git");
+		
+	}
+
+}
